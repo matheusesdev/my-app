efdf147c408b677b9b744c5248e37b1b083b62be
Matheus <matheuses18@icloud.com>
2024-03-21 15:12:59 -0300

    update

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,62 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
+const navLinks = [
+  {
+    title: "About",
+    path: "about",
+  },
+  {
+    title: "Projects",
+    path: "projects",
+  },
+  {
+    title: "Contact",
+    path: "contact",
+  },
+];
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-8 py-2">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5x1 text-white font-semibold"
+        >
+          Logo
+        </Link>
+        <div className=" mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white hover:border-white"
+            >
+              <Bars3Icon className="h-5 w-5"></Bars3Icon>
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white hover:border-white"
+            >
+              <XMarkIcon className="h-5 w-5"></XMarkIcon>
+            </button>
+          )}
+        </div>
+        <div className="menu hidden  md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 sm:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
